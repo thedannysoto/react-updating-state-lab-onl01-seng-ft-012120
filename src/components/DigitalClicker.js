70133285497ba21314fb3453848886912a0d6285
thedannysoto <87dsoto@gmail.com>
2020-05-25 16:24:19 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            timesClicked: 0
+          };
+    }
+
+    handleClick = () => {
+        const incr = this.state.timesClicked + 1;
+        this.setState({
+            timesClicked: incr
+        })
+    }
+
+    render() {
+
+        return(
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        );
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,49 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                resolution: '1080p'
+                }
+            }     
+        }
+    }
+
+    clickBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        }, () => console.log(this.state))
+    }
+
+    clickResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        }, () => console.log(this.state))
+    }
+
+    render() {
+        return(
+            <div>
+            <button className="bitrate" onClick={this.clickBitrate}>Bitrate</button>
+            <button className="resolution" onClick={this.clickResolution}>Resolution</button>
+            </div>
+        );
+    }
+}
